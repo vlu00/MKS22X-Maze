749fb2cce75f886ce41bd54f8aab6d32c494659a
vlu00 <vlu00@stuy.edu>
2019-03-04 14:59:22 -0500

    Tried writing solve using a variable to keep number of @

--- Maze.java
+++ Maze.java
@@ -4,13 +4,14 @@ import java.io.*;
 public class Maze{
   private char[][] maze;
   private boolean animate;
-  private int rows, cols, startRow, startCol;
+  private int rows, cols, startRow, startCol, solution;
   private String tempMaze;
   int[] changeRow = new int[] {-1, 0, 0, 1};
   int[] changeCol = new int[] {0, 1, -1, 0};
 
   public Maze(String filename) throws FileNotFoundException{
     animate = false;
+    solution = -1;
     rows = 0;
     cols = 0;
     tempMaze = "";
@@ -43,54 +44,43 @@ public class Maze{
 
   public int solve() {
     maze[startRow][startCol] = '@';
-    return solve(startRow, startCol, 1);
+    return solve(startRow, startCol);
   }
 
-  public int solve(int row, int col, int solution) {
+  public int solve(int row, int col) {
     if(animate){
       clearTerminal();
+      char save = maze[row][col];
+      maze[row][col] = '\u2588';
       System.out.println(this);
       System.out.println(solution);
-      System.out.println("row " + row);
-      System.out.println("col" + col);
+      maze[row][col] = save;
       wait(1000);
     }
     if (maze[row][col] == 'E') {
-      return solution + 1;
+      return solution + 2;
     }
     else {
-
-      for (int i = 0; i < 4; i++) {
-        char next = maze[row+changeRow[i]][col+changeCol[i]];
-        if (next == ' ') {
-          maze[row][col] = '@';
-          int s = solve(row+changeRow[i], col+changeCol[i], solution+1);
-          if (s != -1) {
-            return solve(row+changeRow[i], col+changeCol[i], solution+1);
-          }
-          maze[row][col] = '.';
-        }
-      }
-      return -1;
-    }
-
-      /*
       for (int i = 0; i < 4; i++) {
         char next = maze[row+changeRow[i]][col+changeCol[i]];
         if (next == ' ') {
           maze[row+changeRow[i]][col+changeCol[i]] = '@';
-          if (solve(row+changeRow[i], col+changeCol[i], solution+1) != -1) {
-            int s = solve(row+changeRow[i], col+changeCol[i], solution+1);
+          solution += 1;
+          int s = solve(row+changeRow[i], col+changeCol[i]);
+          if (s != -1) {
             return s;
           }
           else {
             maze[row+changeRow[i]][col+changeCol[i]] = '.';
+            solution = solution -1;
           }
+        }
+        else if (next == 'E') {
+          return solve(row+changeRow[i], col+changeCol[i]);
         }
       }
     }
     return -1;
-    */
   }
 
   public String toString() {
